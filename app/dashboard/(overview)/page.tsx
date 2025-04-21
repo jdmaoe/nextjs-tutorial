4872bb7a50e932add90b593158d4340f6e053f84
jdmaoe <s.koki2909@gmail.com>
2025-04-21 11:22:13 +0900

    検索機能の実装

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -10,7 +10,7 @@ import {
 import CardWrapper from '@/app/ui/dashboard/cards';
 
 export default async function Page() {
-  return (
+  return (  
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
