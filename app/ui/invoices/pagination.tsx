ee528a8fa76f7a461ddbadb0a9ad2c2b3f44fec0
jdmaoe <s.koki2909@gmail.com>
2025-04-26 16:49:23 +0900

    第12章　修了

--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -4,7 +4,7 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
-import { usePathname, useSearchParams } from 'next/navigation'
+import { usePathname, useSearchParams } from 'next/navigation';
 import { serialize } from 'v8';
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
@@ -12,10 +12,10 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
   const searchParams = useSearchParams();
   const currentPage = Number(searchParams.get('page')) || 1;
 
-  const createPageURL = (pageNumber: Number | string) => {
+  const createPageURL = (pageNumber: number | string) => {
     const params = new URLSearchParams(searchParams);
     params.set('page', pageNumber.toString());
-    return `${pathname}?${params.toString}`; 
+    return `${pathname}?${params.toString()}`; 
   };
   // NOTE: Uncomment this code in Chapter 11
 
